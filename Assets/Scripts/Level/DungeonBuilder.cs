7a41013ee9df3685cf045e448a7516f7791dbd93
JamesK-XIV <jameskidman14@gmail.com>
2025-03-26 14:12:32 +1100

    Implemented system to set rooms to active only when the player is about to enter them

--- Assets/Scripts/Level/DungeonBuilder.cs
+++ Assets/Scripts/Level/DungeonBuilder.cs
@@ -11,21 +11,48 @@ public class DungeonBuilder : MonoBehaviour
     [SerializeField] GameObject finalRoom;
     public int roomCount;
     private int currentRoom = 1;
+    public GameObject[] spawnedRooms;
 
     //Instantiates rooms based on the set roomCount, always ending with the final room
     void GenerateRooms()
     {
         while (currentRoom < roomCount - 1)
         {
-            Instantiate(rooms[Random.Range(0, rooms.Length)]).transform.position += new Vector3(0,25*currentRoom,0);
+            spawnedRooms[currentRoom] = Instantiate(rooms[Random.Range(0, rooms.Length)]);
+            spawnedRooms[currentRoom].transform.position += new Vector3(0, 25 * currentRoom, 0);
+            spawnedRooms[currentRoom].SetActive(false);
             currentRoom += 1;
         }
         Instantiate(finalRoom).transform.position += new Vector3(0, 25 * currentRoom, 0);
     }
+    //Activates rooms when the player reaches the end of the prior room (called upon by DungeonCamera)
+    public void ActivateRooms(int position)
+    {
+        if (spawnedRooms[position + 1] != null)
+        {
+            spawnedRooms[position + 1].SetActive(true);
+            if (spawnedRooms[position + 2] != null)
+            {
+
+                spawnedRooms[position + 2].SetActive(false);
+            }
+        }
+        if (spawnedRooms[position - 1] != null)
+        {
+            spawnedRooms[position - 1].SetActive(true);
+            if (spawnedRooms[position - 2] != null)
+            {
+
+                spawnedRooms[position - 2].SetActive(false);
+            }
+        }
+    }
 
     void Start()
     {
+        spawnedRooms = new GameObject[roomCount];
         GenerateRooms();
+         
     }
 
     void Update()
--- Assets/Scripts/Level/DungeonCamera.cs
+++ Assets/Scripts/Level/DungeonCamera.cs
@@ -1,21 +1,26 @@
 // AUTHOR: James
 // Handles moving the camera when moving between dungeon rooms
 
+using System;
 using UnityEngine;
 
 public class DungeonCamera : MonoBehaviour
 {
+    private DungeonBuilder dungeonBuild;
     // Moves the camera to the position of the room that the player has just entered
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Camera.main.transform.position = transform.parent.transform.position + new Vector3(0,0,-10);
+            Debug.Log((int)transform.position.y / 25);
+            dungeonBuild.ActivateRooms((int)transform.position.y / 25);
+            
         }
     }
     void Start()
     {
-        
+        dungeonBuild = FindAnyObjectByType<DungeonBuilder>();
     }
 
     // Update is called once per frame
