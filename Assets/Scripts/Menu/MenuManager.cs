077ca2d20f10656db0095747afa449bf1db53a3e
Zac <zachary.slater@student.uts.edu.au>
2025-03-27 08:31:15 +1100

    As a player, I want a simple to use main menu so that I know which buttons get me to where I want to go.  & As a player, I want an appealing main menu so that I already start to enter the 'game world' before the game play has even began.

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainMenu;
+    [SerializeField]
+    private GameObject setupMenu;
+    [SerializeField]
+    private GameObject optionsMenu;
+    
+    public void LoadMainMenu()
+    {
+        mainMenu.SetActive(true);
+        setupMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+    }
+
+    public void LoadSetupMenu()
+    {
+        setupMenu.SetActive(true);
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+    }
+
+    public void LoadOptionsMenu()
+    {
+        optionsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+        setupMenu.SetActive(false);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
