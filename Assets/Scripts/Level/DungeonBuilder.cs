ec014b797808646906078a3f5667fe2cd7af9151
JamesK-XIV <jameskidman14@gmail.com>
2025-03-29 21:53:29 +1100

    Revert "Added multiplayer system for moving between rooms"
    This reverts commit 8aa7b8f7ea7d7c3dd8e46df23b4fd96645bf21e2.

--- Assets/Scripts/Level/DungeonBuilder.cs
+++ Assets/Scripts/Level/DungeonBuilder.cs
@@ -9,14 +9,14 @@ public class DungeonBuilder : MonoBehaviour
     [SerializeField] GameObject[] rooms;
     [SerializeField] GameObject startRoom;
     [SerializeField] GameObject finalRoom;
-    public int numberOfRooms;
+    public int roomCount;
     private int currentRoom = 1;
     private GameObject[] spawnedRooms;
 
     //Instantiates rooms based on the set roomCount, always ending with the final room
     void GenerateRooms()
     {
-        while (currentRoom < numberOfRooms - 1)
+        while (currentRoom < roomCount - 1)
         {
             spawnedRooms[currentRoom] = Instantiate(rooms[Random.Range(0, rooms.Length)]);
             spawnedRooms[currentRoom].transform.position += new Vector3(0, 25 * currentRoom, 0);
@@ -30,22 +30,10 @@ public class DungeonBuilder : MonoBehaviour
     //Activates rooms when the player reaches the end of the prior room (called upon by DungeonCamera)
     public void ActivateRooms(int position)
     {
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-        dManager.SetRoomCount(position+1);
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
->>>>>>> Stashed changes
         if (position+1<roomCount && spawnedRooms[position + 1] != null)
-=======
-        dManager.SetRoomCount(position+1);
-        if (position+1<numberOfRooms && spawnedRooms[position + 1] != null)
->>>>>>> Stashed changes
         {
             spawnedRooms[position + 1].SetActive(true);
-            if (position + 2 < numberOfRooms && spawnedRooms[position + 2] != null)
+            if (position + 2 < roomCount && spawnedRooms[position + 2] != null)
             {
 
                 spawnedRooms[position + 2].SetActive(false);
@@ -54,7 +42,7 @@ public class DungeonBuilder : MonoBehaviour
         if (position-1>0 && spawnedRooms[position - 1] != null)
         {
             spawnedRooms[position - 1].SetActive(true);
-            if (position - 2 < numberOfRooms && spawnedRooms[position - 2] != null)
+            if (position - 2 < roomCount && spawnedRooms[position - 2] != null)
             {
 
                 spawnedRooms[position - 2].SetActive(false);
@@ -64,7 +52,7 @@ public class DungeonBuilder : MonoBehaviour
 
     void Start()
     {
-        spawnedRooms = new GameObject[numberOfRooms];
+        spawnedRooms = new GameObject[roomCount];
         GenerateRooms();
          
     }
