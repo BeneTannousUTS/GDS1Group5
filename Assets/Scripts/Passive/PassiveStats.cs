2279c28b0718b177af60cc0a7361ba24a8814d1f
Benedict Tannous laptop <benedict.m.tannous@student.uts.edu.au>
2025-04-10 13:26:38 +1000

    Started scripting for lifesteal

--- Assets/Scripts/Passive/PassiveStats.cs
+++ Assets/Scripts/Passive/PassiveStats.cs
@@ -9,6 +9,9 @@ public class PassiveStats : MonoBehaviour
     //float used for cooldown multipliers
     public float cooldownMultiplier;
     
+    //float used for cooldown multipliers
+    public float lifestealMultiplier;
+    
     //bool used for revive player
     public bool revivePlayer;
 
@@ -34,5 +37,10 @@ public class PassiveStats : MonoBehaviour
     public float GetCooldownMod()
     {
         return cooldownMultiplier;
+    }
+
+    public float GetLifestealMod()
+    {
+        return lifestealMultiplier;
     }
 }
