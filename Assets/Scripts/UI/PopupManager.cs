f564a80a5a078e0a913acf12fbf273fcf45cac9b
Zac <zachary.slater@student.uts.edu.au>
2025-05-11 14:32:48 +1000

    As a player, I want text prompting me to use my weapon in the first room and secondary in the second room so that I know how to attack and use my secondary.

--- Assets/Scripts/UI/PopupManager.cs
+++ Assets/Scripts/UI/PopupManager.cs
@@ -4,7 +4,10 @@ public class PopupManager : MonoBehaviour
 {
     [SerializeField]
     GameObject smallPopupPrefab;
+    [SerializeField]
+    GameObject largePopupPrefab;
     float smallPopupTime = 1.2f;
+    float largePopupTime = 5.0f;
 
     public void SpawnSmallPopup(GameObject entityObject, string text, Color colour)
     {
@@ -12,5 +15,12 @@ public class PopupManager : MonoBehaviour
         smallPopup.transform.position = entityObject.transform.position;
         smallPopup.GetComponent<PopupHandler>().SetText(text, colour);
         Destroy(smallPopup, smallPopupTime);
+    }
+
+    public void SpawnLargePopup(string text, Color colour)
+    {
+        GameObject largePopup = Instantiate(largePopupPrefab, transform.position, transform.rotation);
+        largePopup.GetComponent<PopupHandler>().SetText(text, colour);
+        Destroy(largePopup, largePopupTime);
     }
 }
