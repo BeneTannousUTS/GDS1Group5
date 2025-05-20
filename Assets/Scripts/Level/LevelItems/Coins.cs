41c233270968249d1c39cf1d3eec4b1431037802
Mesmaa <60990360+Julianm-25@users.noreply.github.com>
2025-05-20 13:39:05 +1000

    coin implementation

--- Assets/Scripts/Level/LevelItems/Coins.cs
+++ Assets/Scripts/Level/LevelItems/Coins.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class Coins : MonoBehaviour
 {
     [SerializeField] Sprite[] coinSprites;
+    [SerializeField] GameObject coinSparkle;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<PlayerScore>().AddScore(500);
+            Instantiate(coinSparkle, gameObject.transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
     }
